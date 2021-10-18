33583ffc4181f2d0b5eaf7f4d213b8926f6b37dd
germanseoane <german@rumbosturismo.com>
2021-10-18 16:29:42 -0300

    design changed

--- src/components/InsuranceLogos.js
+++ src/components/InsuranceLogos.js
@@ -17,31 +17,24 @@ const InsuranceLogos = () => {
       Obama Care: <br />
       <br />
       <div className="question-logo">
-        <h4> Ambetter </h4>
         <img src={ambetter} alt="ambetter" className="insurance-image" />
       </div>
       <div className="question-logo">
-        <h4> Oscar </h4>
         <img src={oscar} alt="oscar" className="insurance-image" />
       </div>
       <div className="question-logo">
-        <h4> Florida Blue </h4>
         <img src={floridaBlue} alt="florida" className="insurance-image" />
       </div>
       <div className="question-logo">
-        <h4> Bright Health </h4>
         <img src={brightHealth} alt="bright" className="insurance-image" />
       </div>
       <div className="question-logo">
-        <h4> Molina Health Care </h4>
         <img src={molina} alt="molina" className="insurance-image" />
       </div>
       <div className="question-logo">
-        <h4> Aetna </h4>
         <img src={aetna} alt="aetna" className="insurance-image" />
       </div>
       <div className="question-logo">
-        <h4> Cigna </h4>
         <img src={cigna} alt="signa" className="insurance-image" />
       </div>
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,25 +3,9 @@ import "./Navbar.css";
 import logo from "../assets/logo.jpeg";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Link, useHistory } from "react-router-dom";
-import {
-  OBAMACARE,
-  INSCRIPTION,
-  QUALIFY,
-  SERVICES,
-  BENEFITS,
-  COST,
-  WHEN,
-  WORK,
-} from "../utils/constants";
 
 const Navbar = ({ active, setActive, setQuestion }) => {
   const history = useHistory();
-
-  const handleClick = (value) => {
-    if (value === "DEFAULT") return;
-    history.push("/questions");
-    setQuestion(value);
-  };
 
   return (
     <div className="main-navbar">
@@ -35,34 +19,9 @@ const Navbar = ({ active, setActive, setQuestion }) => {
       <Link to="/" className="nav-link">
         <h3>Sobre Nosotros</h3>
       </Link>
-      <select
-        className="first-select"
-        onChange={(e) => handleClick(e.target.value)}
-      >
-        <option value={"DEFAULT"}>Preguntas frecuentes</option>
-        <option value={OBAMACARE}>Que es Obamacare?</option>
-
-        <option value={INSCRIPTION}>Cual es el periodo de inscripcion?</option>
-
-        <option value={QUALIFY}>Que requisitos necesito para calificar?</option>
-
-        <option value={SERVICES}>
-          Qué servicios y coberturas brinda el seguro médico Obama care?
-        </option>
-        <option value={BENEFITS}>
-          Qué beneficios tiene el plan de seguros Obama Care?
-        </option>
-        <option value={COST}>
-          Qué costo tiene el plan de seguro médico Obama Care?
-        </option>
-        <option value={WHEN}>
-          A partir de cuándo puedo empezar a usar mi cobertura de salud?
-        </option>
-        <option value={WORK}>
-          Qué compañías de seguros trabajan con Obamacare en Florida?
-        </option>
-      </select>
-
+      <Link to="/full" className="nav-link">
+        <h3>Preguntas Frecuentes</h3>
+      </Link>
       <Link to="/contact" className="nav-link">
         <h3>CONTACTENOS</h3>
       </Link>
