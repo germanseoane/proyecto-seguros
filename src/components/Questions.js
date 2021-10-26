6a8585be19c5a2677d897b9fc755cc2915e4f241
germanseoane <german@rumbosturismo.com>
2021-10-26 16:13:30 -0300

    tilde added

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -11,17 +11,19 @@ const Questions = () => {
     <div>
       <div className="question-container">
         <div className="obamacare-container">
-          <h1>¿Que es Obamacare?</h1>
+          <h1>¿Qué es Obamacare?</h1>
           <img alt="obamacare" src={obamacareLogo} className="obama-logo" />
         </div>
         <p>
           Obamacare asigna un subsidio a las familias para ayudarles a pagar su
-          seguro médico. <br />
+          seguro médico. Es un gran beneficio al cual tenemos derecho, por ley.
+          <br />
           <br />
           El seguro Obamacare es el mismo que el seguro privado, la diferencia
-          entre Obama Care y un seguro privado es que si usted califica, se le
-          otorga el subsidio para pagar parte del valor del seguro, mientras que
-          en el seguro privado lo debe pagar por completo. <br />
+          entre Obamacare y un seguro privado es que si usted califica, se le
+          otorga el subsidio de hasta el 100% para pagar el valor del seguro,
+          mientras que en el seguro privado lo debe pagar por completo.
+          <br />
           <br />
           Con el plan Obamacare usted no accede a un seguro de salud sino a un
           crédito fiscal del gobierno que subsidia los altos costos de los
@@ -36,7 +38,11 @@ const Questions = () => {
           toma en consideración la situación familiar, la edad, el hogar y los
           ingresos de cada cliente. <br />
           <br />
-          Contáctenos y le asesoraremos para brindarle la cobertura
+          Bien trabajado y con buena asesoría , los planes de gobierno de
+          Obamacare, son de los mejores beneficios que tenemos como personas
+          dentro de Estados Unidos.
+          <br />
+          <br /> Contáctenos y le asesoraremos para brindarle la cobertura
           médica/seguro que mejor se adapte a usted y su familia según sus
           necesidades.
         </p>
@@ -50,13 +56,16 @@ const Questions = () => {
       <div className="question-container">
         <h1>¿Cual es el periodo de inscripcion?</h1>
         <p>
-          El período de inscripción para el seguro Obama care 2022 (Obamacare
+          El período de inscripción para el seguro Obamacare 2022 (Obamacare
           Open Enrollment) comienza el 1° de noviembre y finaliza el 15 de
-          diciembre. Fuera de esos dos meses usted solo puede obtener una
-          aseguranza médica con carácter excepcional. <br />
+          diciembre del 2021. <br />
           <br />
-          Tiene que haber perdido cobertura por su trabajo o por Medicaid o
-          haber cambiado de estatus migratorio.
+          De todas maneras hay muchos casos, en los cuales usted se puede
+          afiliar en cualquier mes del año.
+          <br />
+          <br />
+          Si usted no pudo afiliarse en el período de inscripcion abierta, no
+          dude en contactarse con nosotros para poder ayudarlo a afiliarse.
         </p>
         <button
           className="question-button"
@@ -69,23 +78,16 @@ const Questions = () => {
         <h1>¿Que requisitos necesito para calificar?</h1>
         <p>
           _Tener un estatus migratorio legal en Estados Unidos (Permiso de
-          _trabajo, residencia, ciudadanía)
+          trabajo, residencia, ciudadanía).
           <br />
-          <br /> _No contar con ingresos anuales superiores a $ 51.520
+          <br /> _No tener cobertura médica a través de su empleador.
           <br />
-          <br /> _No tener cobertura médica a través de su empleador
-          <br />
-          <br /> _No estar en prisión
-          <br />
-          <br />
-          _Aplicar durante el periodo de inscripción para Obamacare 2022 (1° de
-          Noviembre al 15 de diciembre)
         </p>
         <button
           className="question-button"
           onClick={() => history.push("/contact")}
         >
-          Contáctenos
+          Calificar Ahora
         </button>
       </div>
       <div className="question-container">
@@ -100,13 +102,18 @@ const Questions = () => {
           <br />
           _Consultas de especialistas <br />
           <br />
-          _Análisis de laboratorio Terapias de rehabilitación Cuidados
-          preventivos <br />
+          _Consultas de medicos primarios <br /> <br />
+          _Análisis de laboratorio
+          <br />
+          <br /> _Terapias de rehabilitación
+          <br /> <br />
+          _Cuidados preventivos <br />
           <br />
           _Medicamentos recetados por el doctor <br />
           <br />
-          _Ingresos hospitalarios Permite la atención psicológica y cuidados
-          preventivos
+          _Ingresos hospitalarios <br />
+          <br />
+          _Permite la atención psicológica y cuidados preventivos
         </p>
         <button
           className="question-button"
@@ -122,6 +129,14 @@ const Questions = () => {
           hasta los 26 años.
           <br />
           <br />
+          Permite ver a su doctor primario desde 0 dolar la visita.
+          <br />
+          <br />
+          Permite ver a su especialista desde 5 dolares la visita. <br />
+          <br />
+          Permite tener un deducible de 0 dolar.
+          <br />
+          <br />
           Evita que las empresas aseguradoras le nieguen la cobertura o le
           cobren más según su estado de salud, lo descalifiquen si está enfermo
           o si comete algún error honesto en su solicitud. <br />
@@ -131,26 +146,9 @@ const Questions = () => {
           Impide que las compañías aseguradoras impongan incrementos
           injustificados en las cuotas.
           <br /> <br />
-          Elimina los límites de vida y los límites anuales de cobertura en
-          dólares. <br />
-          <br />
-          Viabiliza una rápida apelación a las decisiones de las compañías
-          aseguradoras.
-          <br />
-          <br />
           Extiende la cobertura a decenas de millones de personas mediante el
           subsidio de costos de seguros médicos a través del Mercado de Seguros
           Médicos (HealthCare.Gov y los mercados de seguros estatales). <br />
-          <br />
-          Extiende Medicaid a millones de ciudadanos en los Estados Unidos que
-          así lo precisan y los mejora a los adultos mayores. <br />
-          <br />
-          Proporciona exenciones en impuestos a los negocios pequeños por
-          ofrecer seguro médico a sus empleados. <br />
-          <br />
-          Requiere a los negocios grandes que aseguren a sus empleados, incluso
-          aquellos con condiciones preexistentes. Facilita la asequibilidad a
-          CHIP para los niños. <br />
           <br />
           Asegura que todos los planes cubran los beneficios mínimos con límites
           en los costos y ofrece diez beneficios esenciales.
@@ -166,32 +164,18 @@ const Questions = () => {
         <h1>¿Qué costo tiene el plan de seguro médico Obama Care?</h1>
         <p>
           Los precios de un seguro médico de Obamacare están entre los 0 USD y
-          los 75 USD. <br />
+          los 45 USD mensuales. <br />
           <br />
-          Las prestaciones de servicios de alta calidad rondan los 30 dólares
+          Las prestaciones de servicios de alta calidad rondan los 20 dólares
           mensuales en un plan Silver. <br />
           <br />
           Por supuesto, esto varía según cada caso: mientras mayor sea el
           ingreso familiar, menor será el subsidio del gobierno en el seguro
           médico Obamacare. <br />
           <br />
-          <br />
           Si sus ingresos anuales califican podría tener un seguro médico sin
-          costo. Con un plan tipo bronce, se beneficia de servicios de salud sin
-          pagar ni un centavo. <br />
+          costo.
           <br />
-          Eso sí, recuerde que su deducible de hospital será alto. No obstante,
-          siempre recomendamos aplicar para Obamacare en español.
-          <br />
-          <br />
-          Si ya cuenta con la documentación necesaria visite nuestra agencia en
-          7216 SW 8th St Ste 5 Miami Fl 33144. De esta manera podrá comenzar el
-          proceso de solicitud. <br />
-          <br />
-          También puede comunicarse con nosotros a través del 786-910-9487 donde
-          uno de nuestros agentes le atenderá con gusto. <br />
-          <br />
-          El servicio de Obamacare 2022 en Florida es libre de costo.
         </p>
         <button className="question-button">Contáctenos</button>
       </div>
@@ -203,14 +187,12 @@ const Questions = () => {
         <p>
           Si aplica para un seguro de salud Obamacare en el período de
           inscripción abierta, usted contará con cobertura a partir del 1 de
-          enero. <br />
+          enero.
           <br />
-          En cambio, si ha ocurrido un cambio en su vida, y califica para una
-          Inscripción Especial su cobertura estará vigente el día 1 del mes
-          siguiente. <br />
           <br />
-          Solo a partir de esta fecha, usted podrá contar con los beneficios de
-          su cobertura médica.
+          De lo contrario, empezaría el primer dia del mes siguiente de que haya
+          hecho su solicitud.
+          <br />
         </p>
         <button
           className="question-button"
